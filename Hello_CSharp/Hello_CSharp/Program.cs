3d35fd1f0913971eb8a0b25c0647487b79585640
sir_rigel <sir.rigel@gmail.com>
2016-08-22 18:36:25 -0500

    Initial commit, Welp time to learn c#, I guess ...

---
+++ Hello_CSharp/Hello_CSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;//Oh no ...
+using System.Collections.Generic;//So this is how we used libraries in java ... I think (It's been 10 years
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_CSharp
+{
+    class Program//Oh god, the Java-like begins
+    {
+        static void Main(string[] args)//I guess we are declaring Functions in very much the same way as Java
+        {
+            helloWorld();//Well, Ruby/Swift has thourally screwed me up ... we need semicolins, Crap ... also how we run a function
+        }
+        static void helloWorld()
+        {
+            Console.WriteLine("Hello World!");//Well this is pretty interesting ... how to write to console
+            Console.ReadKey();//This is annoying, ah well, might as well keep it for now ... though
+        }
+    }
+}
