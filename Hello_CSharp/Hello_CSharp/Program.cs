65dcb12e6b6f8e4b3e1cfc92715fa717d22317fc
sir_rigel <sir.rigel@gmail.com>
2016-08-22 18:52:59 -0500

    =Playing around with some basic flow control, ifs and things like that

--- Hello_CSharp/Hello_CSharp/Program.cs
+++ Hello_CSharp/Hello_CSharp/Program.cs
@@ -11,11 +11,27 @@ namespace Hello_CSharp
         static void Main(string[] args)//I guess we are declaring Functions in very much the same way as Java
         {
             helloWorld();//Well, Ruby/Swift has thourally screwed me up ... we need semicolins, Crap ... also how we run a function
+            ifTesting(true);
+            ifTesting(false);//Awesome, default options work the same, so this shouldn't be too bad
+            ifTesting(false, true);
+            Console.ReadKey();//This is annoying, ah well, might as well keep it for now ... though
         }
         static void helloWorld()
         {
             Console.WriteLine("Hello World!");//Well this is pretty interesting ... how to write to console
-            Console.ReadKey();//This is annoying, ah well, might as well keep it for now ... though
+        }
+        static void ifTesting(bool testVar1, bool testVar2=false)//Alright this is how you feed in a single variable
+        {
+            if (testVar1)//And this is how we do conditional testing
+            {
+                Console.WriteLine("If you do not bend...you break.");
+            }
+            else if(testVar2){//And an additional Layer
+                Console.WriteLine("I am on Fire, but an extinguisher is not required");
+            }else
+            {
+                Console.WriteLine("Adversity is an opportunity for change.");
+            }
         }
     }
 }
