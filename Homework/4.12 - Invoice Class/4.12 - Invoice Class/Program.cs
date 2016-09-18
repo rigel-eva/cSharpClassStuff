5f2246ec6a58426b5324e6992a30b967dc9fbb9a
sir_rigel <sir.rigel@gmail.com>
2016-09-18 14:19:37 -0500

    4.12 - Initial Commit

---
+++ Homework/4.12 - Invoice Class/4.12 - Invoice Class/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._12___Invoice_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
