129c85c6b4a6d5707a31f066a63469bf0d75b885
sir_rigel <sir.rigel@gmail.com>
2016-09-18 17:17:11 -0500

    Welp, it was easier to copy and slightly modify the date that I made in class instead of rewriting it from scratch ...

---
+++ Homework/4.14 - Date/4.14 - Date/Date.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16._9._12___Dates_and_Imaginary_numbers
+{
+    class Date
+    {
+        public int Day { get; private set; }
+        public int Month { get; private set; } 
+        public int Year { get; private set; }
+        Date(int year, int month, int day)
+        {
+            Year = year;
+            Month = month;
+            Day = day;
+        }
+       override public string ToString()
+        {
+            return $"{Month:d2}/{Day:d2}/{Year:d2}";//Well that's neat, string interpolation works here, though not exactly the same as Ruby.
+        }
+        public void DisplayDate()
+        {
+            Console.WriteLine(this.ToString());
+        }
+    }
+}
