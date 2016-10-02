63058814c0727a5046911ae4507299d5700749ef
sir_rigel <sir.rigel@gmail.com>
2016-10-02 16:09:58 -0500

    Well ... that didn't take too long ... dear freaking christ talk about line counts ... dear god ...

---
+++ Homework/5.29/5.29/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(box(getInput("Width"), getInput("Height")));
+
+        }
+        static int getInput(string prompt)
+        {
+            int returner;
+            string input;
+            Console.Write(prompt + ": ");
+            input = Console.ReadLine();
+            if(int.TryParse(input,out returner))
+            {
+                return returner;
+            }
+            else
+            {
+                Console.WriteLine("Please enter an integer.");
+                return getInput(prompt);
+            }
+        }
+        static string box(int width, int height)//Well, might as well make a function for this ...
+        {
+            string returner = "";
+
+            returner += line(width, true);
+            for(int i=0; i<height-2; i++)
+            {
+                returner += line(width, false);
+            }
+            returner += line(width, true);
+            return returner;
+        }
+        static string line(int width, bool solid = false)//And I want my code as dry as possible.
+        {
+            string returner = "*";
+            for(int i=0;i<width-2; i++)
+            {
+                if (solid)
+                {
+                    returner += "*";
+                }
+                else
+                {
+                    returner += " ";
+                }
+            }
+            returner += "*\n";
+            return returner;
+        }
+    }
+}
