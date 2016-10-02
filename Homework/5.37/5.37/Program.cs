3345b982de8f46d66d5a759480f347bd4603d896
sir_rigel <sir.rigel@gmail.com>
2016-10-02 16:26:40 -0500

    So, 5.37 is done ... and holy crap I need to stop with the one line main thing ... It's nice and all, but JEZUS.

---
+++ Homework/5.37/5.37/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Factorial of that number is: " + factorial(getInput("Please enter a positive integer")));
+        }
+        static uint getInput(string prompt)//converted this function to uint, because, unsigned integers are fun, right? (also, instead of checking whether it's positive or not, I can just check if it's a valid unsigned integer.)
+        {
+            uint returner;
+            string input;
+            Console.Write(prompt + ": ");
+            input = Console.ReadLine();
+            if (uint.TryParse(input, out returner))
+            {
+                return returner;
+            }
+            else
+            {
+                Console.WriteLine("Please enter a positive integer.");
+                return getInput(prompt);
+            }
+        }
+        static uint factorial(uint integer)
+        {
+            if (integer == 1)
+            {
+                return 1;
+            }
+            return integer * factorial(integer - 1);//Why yes I will turn this into a recursion problem. because why not?
+        }
+    }
+}
