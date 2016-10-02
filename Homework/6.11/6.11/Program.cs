28067f968e5cbeba34e8a5f6d7048bf133c3357c
sir_rigel <sir.rigel@gmail.com>
2016-10-02 16:47:29 -0500

    6.11 finished: And another one gone, another one bites the dust!

---
+++ Homework/6.11/6.11/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfInputs;
+            int smallest=int.MaxValue;
+            do
+            {
+                numberOfInputs = getInput("Please enter the number of values");
+                if (numberOfInputs < 1)
+                {
+                    Console.WriteLine("Please enter a Positive Integer");
+                }
+            } while (numberOfInputs < 1);
+            for(int i= numberOfInputs-1; i>0; i--)
+            {
+                smallest = Math.Min(smallest, getInput("Please enter an Integer"));
+            }
+            Console.WriteLine("The smallest value inputed was: " + smallest);
+        }
+        static int getInput(string prompt)
+        {
+            int returner;
+            string input;
+            Console.Write(prompt + ": ");
+            input = Console.ReadLine();
+            if (int.TryParse(input, out returner))
+            {
+                return returner;
+            }
+            else
+            {
+                Console.WriteLine("Please enter an integer.");
+                return getInput(prompt);
+            }
+        }
+    }
+}
