e4bef933caa3af40c647e865a651b591ee97d6ae
sir_rigel <sir.rigel@gmail.com>
2016-10-03 19:59:13 -0500

    And like that, bug fixed.

--- In_Class/16.10.3 - In Class Exercise/16.10.3 - In Class Exercise/Program.cs
+++ In_Class/16.10.3 - In Class Exercise/16.10.3 - In Class Exercise/Program.cs
@@ -14,7 +14,7 @@ namespace _16._10._3___In_Class_Exercise
         }
         static void exercise1()
         {
-            for(int i=0; i<21; i++)
+            for(int i=1; i<21; i++)
             {
                 Console.WriteLine("1/{0}=={1}", i, 1.0 / i);
             }
