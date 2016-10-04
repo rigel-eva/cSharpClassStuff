c3f5cda21c50801595c86b6638b66164ca9dcf36
sir_rigel <sir.rigel@gmail.com>
2016-10-03 20:08:29 -0500

    Added the second exercise ... kinda boring but meh.

--- In_Class/16.10.3 - In Class Exercise/16.10.3 - In Class Exercise/Program.cs
+++ In_Class/16.10.3 - In Class Exercise/16.10.3 - In Class Exercise/Program.cs
@@ -10,13 +10,48 @@ namespace _16._10._3___In_Class_Exercise
     {
         static void Main(string[] args)
         {
-            exercise1();
+            exercise2();
         }
         static void exercise1()
         {
             for(int i=1; i<21; i++)
             {
                 Console.WriteLine("1/{0}=={1}", i, 1.0 / i);
+            }
+        }
+        static void exercise2()
+        {
+            int smallest = int.MaxValue;
+            int secSmallest = int.MaxValue;
+            for(int i=0; i<5; i++)
+            {
+                int input = getInput("Please enter the " + (i + 1) + " value");
+                if (input < smallest)
+                {
+                    smallest = input;
+                }
+                else if (input < secSmallest)
+                {
+                    secSmallest = input;
+                }
+            }
+
+            Console.WriteLine("Smallest Value: {0}\nSecond Smallest Value: {1}", smallest, secSmallest);
+        }
+        static int getInput(string prompt)// much code reuse. many lazy. wow.
+        {
+            int returner;
+            string input;
+            Console.Write(prompt + ": ");
+            input = Console.ReadLine();
+            if (int.TryParse(input, out returner))
+            {
+                return returner;
+            }
+            else
+            {
+                Console.WriteLine("Please enter an integer.");
+                return getInput(prompt);
             }
         }
     }
