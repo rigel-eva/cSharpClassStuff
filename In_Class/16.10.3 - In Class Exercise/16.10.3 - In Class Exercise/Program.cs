ea72e79ca1c1bb823ac12553144a49e16897b5d6
sir_rigel <sir.rigel@gmail.com>
2016-10-03 19:58:49 -0500

    Finished the first part of the exercise, kind of ... There is a bug here that I find facinating.

--- In_Class/16.10.3 - In Class Exercise/16.10.3 - In Class Exercise/Program.cs
+++ In_Class/16.10.3 - In Class Exercise/16.10.3 - In Class Exercise/Program.cs
@@ -10,6 +10,14 @@ namespace _16._10._3___In_Class_Exercise
     {
         static void Main(string[] args)
         {
+            exercise1();
+        }
+        static void exercise1()
+        {
+            for(int i=0; i<21; i++)
+            {
+                Console.WriteLine("1/{0}=={1}", i, 1.0 / i);
+            }
         }
     }
 }
