5cd50328f1fed41e03277d35f4082bb9a34a0cc4
sir_rigel <sir.rigel@gmail.com>
2016-10-03 19:54:26 -0500

    Started In Class Exersize for 10/3/16

---
+++ In_Class/16.10.3 - In Class Exercise/16.10.3 - In Class Exercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16._10._3___In_Class_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
