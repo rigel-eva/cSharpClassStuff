4d3fa11246e59f5b832855c35ad08544573affb9
sir_rigel <sir.rigel@gmail.com>
2016-10-03 19:01:13 -0500

    Ok, so interest rate caluclator is commented out, and a rudementary menu ... yay?

--- In_Class/16.10.3/16.10.3/Program.cs
+++ In_Class/16.10.3/16.10.3/Program.cs
@@ -10,6 +10,31 @@ namespace _16._10._3
     {
         static void Main(string[] args)
         {
+            decimal rate;
+            decimal aT=1;
+            int time = 0;
+            int selection=int.MinValue;
+            do
+            {
+                do
+                {
+                    Console.Clear();//Want to make sure the console is clear before we start executing code
+                    Console.Write("1) Option 1\n" +
+                                      "2) Option 2\n" +
+                                      "3) Option 3\n" +
+                                      ">");
+                } while (!int.TryParse(Console.ReadLine(), out selection));
+            } while ((selection < 1 || selection > 3));
+            //do
+            //{
+            //    Console.Write("Eneter the interest rate as a decimal Value: ");
+            //} while (!Decimal.TryParse(Console.ReadLine(),out rate));
+            //while (aT < 2)
+            //{
+            //    time++;
+            //    aT = aT * (1 + rate);
+            //}
+            //Console.WriteLine("It would take {0} units of time to double the value.", time);
         }
     }
 }
