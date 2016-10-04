18d53567c6759ce2da9a2e2868ba810a50d0d150
sir_rigel <sir.rigel@gmail.com>
2016-10-03 19:40:18 -0500

    added an example of what the infinite loop might look like ...

--- In_Class/16.10.3/16.10.3/Program.cs
+++ In_Class/16.10.3/16.10.3/Program.cs
@@ -25,31 +25,31 @@ namespace _16._10._3
                                       ">");
                 } while (!int.TryParse(Console.ReadLine(), out selection));
             } while ((selection < 1 || selection > 3));
-            //So I could write a switch here, but ... essentially the only thing that's different from c++ is that you can't do weird things ... like not having breaks.
-            switch (10 / 10)
-            {
-                case 9:
-                case 10:
-                case 11:
-                    Console.WriteLine("Are you sure this is correct?");
-                    goto case 12;//THIS CAN CAUSE AN INFINITE LOOP >_<
-                case 12:
-                case 13:
-                    Console.WriteLine("You Got an A!");
-                    break;
-                case 8:
-                    Console.WriteLine("You Got a B!");
-                    break;
-                case 7:
-                    Console.WriteLine("You got a C.");
-                    break;
-                case 6:
-                    Console.WriteLine("You got a D ...");
-                    break;
-                default:
-                    Console.WriteLine("You got an F ... ");
-                    break;
-            }
+            //switch (10 / 10)
+            //{
+            //    case 9:
+            //    case 10:
+            //    case 11:
+            //        Console.WriteLine("Are you sure this is correct?");
+            //        goto case 12;//THIS CAN CAUSE AN INFINITE LOOP >_<
+            //        //goto case 9;//Like this ... oh God ... X(
+            //    case 12:
+            //    case 13:
+            //        Console.WriteLine("You Got an A!");
+            //        break;
+            //    case 8:
+            //        Console.WriteLine("You Got a B!");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("You got a C.");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("You got a D ...");
+            //        break;
+            //    default:
+            //        Console.WriteLine("You got an F ... ");
+            //        break;
+            //}
             //do
             //{
             //    Console.Write("Eneter the interest rate as a decimal Value: ");
