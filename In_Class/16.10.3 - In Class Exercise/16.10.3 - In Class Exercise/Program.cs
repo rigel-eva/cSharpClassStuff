39cf5d0b3873a0cb2d3e62028ca733412a818ff2
sir_rigel <sir.rigel@gmail.com>
2016-10-03 20:20:25 -0500

    edited my getInput function to add the capability of diferentiating between ints and uints ...
    And Suddenly Fibbonachi

--- In_Class/16.10.3 - In Class Exercise/16.10.3 - In Class Exercise/Program.cs
+++ In_Class/16.10.3 - In Class Exercise/16.10.3 - In Class Exercise/Program.cs
@@ -10,7 +10,7 @@ namespace _16._10._3___In_Class_Exercise
     {
         static void Main(string[] args)
         {
-            exercise2();
+            exercise3();
         }
         static void exercise1()
         {
@@ -25,7 +25,8 @@ namespace _16._10._3___In_Class_Exercise
             int secSmallest = int.MaxValue;
             for(int i=0; i<5; i++)
             {
-                int input = getInput("Please enter the " + (i + 1) + " value");
+                int input;
+                getInput("Please enter the " + (i + 1) + " value", out input);
                 if (input < smallest)
                 {
                     smallest = input;
@@ -38,21 +39,51 @@ namespace _16._10._3___In_Class_Exercise
 
             Console.WriteLine("Smallest Value: {0}\nSecond Smallest Value: {1}", smallest, secSmallest);
         }
-        static int getInput(string prompt)// much code reuse. many lazy. wow.
+        static void getInput(string prompt, out int output)// much code reuse. many lazy. wow.
         {
-            int returner;
             string input;
             Console.Write(prompt + ": ");
             input = Console.ReadLine();
-            if (int.TryParse(input, out returner))
-            {
-                return returner;
-            }
-            else
+            if (!int.TryParse(input, out output))
             {
                 Console.WriteLine("Please enter an integer.");
-                return getInput(prompt);
+                getInput(prompt, out output);
             }
+        }
+        static void getInput(string prompt, out uint output)// much code reuse. many lazy. wow.
+        {
+            string input;
+            Console.Write(prompt + ": ");
+            input = Console.ReadLine();
+            if (uint.TryParse(input, out output))
+            {
+                Console.WriteLine("Please enter an integer.");
+                return getInput(prompt, out output);
+            }
+        }
+        static void exercise3()//Fibbonachi sequence ... fun ...
+        {
+            uint numTerms;
+            uint a=0;
+            uint b=0;
+
+            getInput("Enter the number of terms to get", out numTerms);
+            for(int i=0; i<numTerms; i++)
+            {
+                if (i == 0)
+                {
+                    continue;
+                }
+                else if(i==1)
+                {
+                    b = 1;
+                    continue;
+                }
+                uint temp = a;
+                a = b;
+                b = temp + a;
+            }
+            Console.WriteLine("The {0}th term of the Fibbonachi sequence is {1}", numTerms, b);
         }
     }
 }
