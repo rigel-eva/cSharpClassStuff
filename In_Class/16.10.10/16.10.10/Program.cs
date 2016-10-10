24ceadd069a65068abbcd4bdd3c11402ae955959
sir_rigel <sir.rigel@gmail.com>
2016-10-10 18:23:59 -0500

    New Class period, new stuff ...

---
+++ In_Class/16.10.10/16.10.10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16._10._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
