5a44bbee5d0a570ab8998c01b44cb22bd880a5a2
sir_rigel <sir.rigel@gmail.com>
2016-10-10 18:43:23 -0500

    Well, this is neat ... interfaces!

--- In_Class/16.10.10/16.10.10/Program.cs
+++ In_Class/16.10.10/16.10.10/Program.cs
@@ -8,12 +8,14 @@ namespace _16._10._10
 {
     class Program
     {
-        static int Maximum(int a, int b)
+        static IComparable Maximum(IComparable  a, IComparable b)
         {
-            return (a > b) ? a : b;//And here comes the single line ifs ... oh no ...
+            return (a.CompareTo(b)<0) ? a : b;//And here comes the single line ifs ... oh no ...
         }
         static void Main(string[] args)
         {
+            Console.Write(3.CompareTo(5));
+            Maximum(3,5);
         }
     }
 }
