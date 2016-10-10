19166850b1e0f3b5cccb007617ae06d020bde34f
sir_rigel <sir.rigel@gmail.com>
2016-10-10 18:27:33 -0500

    Well, single line ifs are a thing ... a thing to avoid in most cases (can lead to confusion, in my opinion)

--- In_Class/16.10.10/16.10.10/Program.cs
+++ In_Class/16.10.10/16.10.10/Program.cs
@@ -8,6 +8,10 @@ namespace _16._10._10
 {
     class Program
     {
+        static int Maximum(int a, int b)
+        {
+            return (a > b) ? a : b;//And here comes the single line ifs ... oh no ...
+        }
         static void Main(string[] args)
         {
         }
