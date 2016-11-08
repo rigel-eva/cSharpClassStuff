b344aedf96ac2208b346f6317fe5ad7234b9adfe
sir_rigel <sir.rigel@gmail.com>
2016-11-07 18:57:16 -0600

    Files!
    Working off the laptop for a change, going to see how it changes things ...

---
+++ In_Class/16.11.7.Files/16.11.7.Files/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+namespace Files
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			try
+			{
+				using (StreamWriter stream = new StreamWriter("numbers.txt"))
+				{
+					for (int i = 1; i < 101; i++)
+					{
+						stream.Write(i+"\n");
+					}
+				}
+				using (StreamReader stream = new StreamReader("numbers.txt"))
+				{
+					string name = stream.ReadLine();
+					Console.WriteLine(name);
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Something is amiss...: \n "+e.ToString());
+			}
+		}
+	}
+}
