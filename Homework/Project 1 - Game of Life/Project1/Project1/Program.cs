685a764d0ebda2a395a1f0e861b932ecac004cb5
sir_rigel <sir.rigel@gmail.com>
2016-11-21 00:28:29 -0600

    Ok, conwayCell.cs is working ... so next step: Finding repeats ... almost there too.

--- Homework/Project 1 - Game of Life/Project1/Project1/Program.cs
+++ Homework/Project 1 - Game of Life/Project1/Project1/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-
+using System.Threading;
+using System.Security.Cryptography;
 namespace Project1
 {
 	class MainClass
 	{
+		static conwayCell[,] cells;
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+            cells = conwayCell.initCells(80,24);
+            for(int i=0; i<1000; i++)
+            {
+                Console.WriteLine(conwayCell.writeArray(ref cells));
+                Console.Write("Generation {0}, Current Population: {1}/{2}",i+1, conwayCell.currentPopulation(ref cells),cells.Length);
+                Thread.Sleep(35);
+                conwayCell.step(ref cells);
+                Console.Clear();
+            }
 		}
+        public static int checksum(string toCheck)
+        {
+            int returner=0;
+
+            return returner;
+        }
 	}
 }
--- Homework/Project 1 - Game of Life/Project1/Project1/conwayCell.cs
+++ Homework/Project 1 - Game of Life/Project1/Project1/conwayCell.cs
@@ -1,45 +1,63 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
-class conwayCell{
-  public enum cellStatus{alive, dead, dying, birthing};//All possible states of a cell
-  private List<conwayCell> neighbors;//Our Neighbors
-  private bool _alive;//Whether or not the cell is alive
-  public bool alive{
-    get{
-      return _alive;
-    }
-    private set{
-      _alive=value;
-    }
-  }
-  public cellStatus status{
-    get{
+class conwayCell
+{
+	public enum cellStatus { alive, dead, dying, birthing };//All possible states of a cell
+	private List<conwayCell> neighbors;//Our Neighbors
+	private bool _alive;//Whether or not the cell is alive
+	public bool alive
+	{
+		get
+		{
+			return _alive;
+		}
+		private set
+		{
+			_alive = value;
+		}
+	}
+	public cellStatus status
+	{
+		get
+		{
 			bool nextStep = isAliveNextStep();
-	      if(alive&&nextStep){
-	        return cellStatus.alive;
-	      }
-	      else if(!alive&&nextStep){
-	        return cellStatus.birthing;
-	      } else if(alive&&!nextStep){
-	        return cellStatus.dying;
-	      }
-	      else{
-	        return cellStatus.dying;
-	      }
+			if (alive && nextStep)
+			{
+				return cellStatus.alive;
+			}
+			else if (!alive && nextStep)
+			{
+				return cellStatus.birthing;
+			}
+			else if (alive && !nextStep)
+			{
+				return cellStatus.dying;
+			}
+			else {
+				return cellStatus.dead;
+			}
+		}
+	}
+    #region Constructors
+    public conwayCell()
+	{
+		alive = false;
+        neighbors = new List<conwayCell>();
+	}
+	public conwayCell(bool alive)
+	{
+		this.alive = alive;
+        neighbors = new List<conwayCell>();
     }
-  }
-  public conwayCell(){
-		neighbors = new List<conwayCell>();
-		alive=false;
-  }
-  public conwayCell(bool alive){
-    this.alive=alive;
-  }
-  public void addNeighbor(conwayCell neighbor){
-		this.neighbors.add(neighbor);
-  }
-  private bool isAliveNextStep(){
-    int livingCells=0;
+    #endregion
+    public void addNeighbor(ref conwayCell neighbor)
+	{
+#warning check here first if something goes a bit screwy ... this should allow us to get constant access to our neighbors state ... but just in case.
+        neighbors.Add(neighbor);
+	}
+	private bool isAliveNextStep()
+	{
+		int livingCells = 0;
 		for (int i = 0; i < neighbors.Count; i++)
 		{
 			if (neighbors[i].alive)
@@ -47,6 +65,7 @@ class conwayCell{
 				livingCells++;
 			}
 		}
+        Console.Write("");
 		switch (livingCells)
 		{
 			case 2://Survival
@@ -63,9 +82,94 @@ class conwayCell{
 			default:
 				return false;
 		}
-  }
-  public void step(){
-    alive=isAliveNextStep();
-  }
-
+	}
+	public void step()
+	{
+		alive = isAliveNextStep();
+	}
+    public override string ToString()
+    {
+        if (alive)
+        {
+            return "▓";
+        }
+        else
+        {
+            return "░";
+        }
+    }
+    public static conwayCell[,] initCells(int width, int height)
+	{
+		conwayCell[,] returner = new conwayCell[height, width];
+		Random rand = new Random();
+		for (int i = 0; i < height; i++)
+		{
+			for (int j = 0; j < width; j++)
+			{
+                bool randomBool = rand.NextDouble() >= 0.5;
+                returner[i, j]= new conwayCell(randomBool);//Inits a random value
+			}
+		}
+        for (int i = 0; i < width - 1; i++)
+        {
+            for (int j = 0; j < height - 1; j++)
+            {
+                addNeighbors(ref returner, j,i);
+            }
+        }
+        return returner;
+    }
+    private static void addNeighbors(ref conwayCell[,] cellList, int x, int y)
+    {
+        Console.Write("");
+        for (int i = x - 1; i < x + 2; i++)
+        {
+            for(int j=y-1; j<y+2; j++)
+            {
+                Console.Write("");
+                if (!(i < 0 || i > cellList.GetLength(0)-1 || j < 0 || j > cellList.GetLength(1)-1||(i==x&&j==y)))//As long as we arn't trying to add ourself, or trying to add something out of range ... 
+                {
+                    cellList[x, y].addNeighbor(ref cellList[i, j]);
+                }
+           } 
+        }
+    }
+	public static void step(ref conwayCell[,] cells)
+	{
+		for (int i = 0; i < cells.GetLength(0); i++)
+		{
+			for (int j = 0; j < cells.GetLength(1); j++)
+			{
+				cells[i, j].step();
+			}
+		}
+	}
+    public static string writeArray(ref conwayCell[,] cells)
+    {
+        string returner = "";
+        for(int i=0; i<cells.GetLength(0); i++)
+        {
+            for(int j=0; j<cells.GetLength(1); j++)
+            {
+                returner += cells[i, j];
+            }
+            returner += "\n";
+        }
+        return returner;
+    }
+    public static int currentPopulation(ref conwayCell[,] cells)
+    {
+        int returner = 0;
+        for(int i=0; i<cells.GetLength(0); i++)
+        {
+            for(int j=0; j<cells.GetLength(1); j++)
+            {
+                if (cells[i, j].alive)
+                {
+                    returner++;
+                }
+            }
+        }
+        return returner;
+    }
 }
