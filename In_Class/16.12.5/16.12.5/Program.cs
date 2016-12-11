4faea7e9552b71027307d2f8fdc2273f3533fbba
sir_rigel <sir.rigel@gmail.com>
2016-12-11 17:51:27 -0600

    Just a quick commit

---
+++ In_Class/16.12.5/16.12.5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Application
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int test;
+			Console.Write("Enter a number to divide 32 by ...\n>");
+			if (int.TryParse(Console.ReadLine(), out test))
+			{
+				try
+				{
+					test = 32 / test;
+				}
+				catch (DivideByZeroException e)
+				{
+					Console.WriteLine("Um ... are you sure you want to do that?");
+					Console.WriteLine(e.InnerException.Message);
+				}
+				finally
+				{
+					//Stuff in here will get executed wether or not the try succeeds, like closing files.
+					Console.WriteLine("And this is going to be printed no matter what!");
+				}
+				/*using (String boop= new String())
+				{
+					//Equivilant of try / finally
+				}*/
+			}
+		}
+	}
+}
